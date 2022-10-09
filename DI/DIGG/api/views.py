04bcb1885eed4ba5411674045eda0fc08e25f7f5
eksnew <eks_new@outlook.com>
2022-10-09 16:13:21 +0800

    10.9

--- DI/DIGG/api/views.py
+++ DI/DIGG/api/views.py
@@ -18,11 +18,13 @@ class CodeView(APIView):
         # print(request.data['type'])
         if request.data['type'] == "camera":
             pic_base64 = request.data['base1']
+            #print("pic_base64:", type(pic_base64) ) #  <class 'str'>
             pic = base64.b64decode(pic_base64)  # base64解码为 \x**\x**\x**\x**
+            #print("pic:", type(pic)) # pic: <class 'bytes'>
 
-            nparr = np.fromstring(pic, np.uint8)
-            pic_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # <class 'numpy.ndarray'># 函数从指定的内存缓存中读取数据，并把数据转换(解码)成图像格式;主要用于从网络传输数据中恢复出图像。
-            print("Pic_np Get.")
+            # nparr = np.fromstring(pic, np.uint8)
+            # pic_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # <class 'numpy.ndarray'># 函数从指定的内存缓存中读取数据，并把数据转换(解码)成图像格式;主要用于从网络传输数据中恢复出图像。
+            # print("Pic_np Get.")
             # 通过opencv显示图片
             #resize_img = cv2.resize(pic_np, dsize=(300, 300))
             #cv2.imshow("img", resize_img)
@@ -34,13 +36,20 @@ class CodeView(APIView):
             f.close()
             #print("Image Saved.")
             # 调用模型检测识别
-            result_rcnn = async_inference_detector(store.model_rcnn, pic_np)#'demo.jpg')
-            print(type(result_rcnn))
-            #img = store.model_rcnn.show_result(pic_np, result_rcnn, bbox_color=(0, 255, 0), text_color=(0, 255, 0))
+            # result_rcnn = async_inference_detector(store.model_rcnn, pic_np)#'demo.jpg')
+            result_rcnn = inference_detector(store.model_rcnn, 'demo.jpg')
+            #print(type(result_rcnn)) # <class 'list'>
+            # img = store.model_rcnn.show_result(pic_np, result_rcnn, bbox_color=(0, 255, 0), text_color=(0, 255, 0))
+            img = store.model_rcnn.show_result('demo.jpg', result_rcnn, bbox_color=(0, 255, 0), text_color=(0, 255, 0))
+            #print("result_img:", type(img) )  # result_img: <class 'numpy.ndarray'>
+            retval, img_buffer = cv2.imencode('.jpg', img)
+            img_str = base64.b64encode(img_buffer)
+            result_base64 = img_str
+            # print('result_base64:', result_base64)
             #cv2.imwrite('result1_888.jpg', img)
             print("Result Saved66.")
-            #draft_openmmlab.img_detection("faster")
+
         print("POST结束 return------------------------------------------------------------------")
-        return Response({"state": True, "tips": "You visited api-login-views-CodeView-post"})# 返回值一个字典
+        return Response({"state": True, "result_base64": result_base64})# 返回值一个字典
 
 
