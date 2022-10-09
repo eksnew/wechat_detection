05fdcf84cdcf5cb63b036e3b7c1f7491144bdbbc
A_teng <1114134095@qq.com>
2022-10-09 22:38:23 +0800

    三种检测模型均实现

--- miniprogram-first/pages/camera/camera.js
+++ miniprogram-first/pages/camera/camera.js
@@ -1,26 +1,47 @@
 // pages/camera/camera.js
 const { set } = require("mobx-miniprogram");
 var common=require("../../utils/common")
+
+import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
+import { store } from '../../store/store'
+
 Page({
+
+  //全局变量绑定
+  //挂载一下全局变量
+  behaviors:[storeBindingsBehavior],
+  storeBindings:{
+    store,
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-      code:"",
-      times:1,
-      result_base64:"" //检测后的结果图片的base64格式
+      code:"",   //相机获得的相片base64格式
+      times:1,   //定时器计数变量
+      result_base64:"", //检测后的结果图片的base64格式
+      request_allow:1, //是否允许向服务器发送request_POST请求
+      methodName:""
   },
 
   /**
    * 点击拍照函数
    */
   getPhoto:function(){
+      this.setData({
+        'request_allow': 1
+      })
       var flag1 = 1
       this.data.times = flag1
       var timer = setInterval(()=>{ //建立计时器
         var that = this //创建一个名为that的变量来保存this当前的值  
          const ctx = wx.createCameraContext()  //获得创建照相机的容器
+         if (that.data.request_allow == 1)
+         {
+           that.setData({
+             'request_allow': 0
+           })
          ctx.takePhoto({
             quality:'low', //质量
             success:(res) =>{
@@ -32,14 +53,15 @@ Page({
               })
             }
           })
+                 
           wx.request({     
-          //url: 'http://10.198.67.131:8088/api/login/',  
-          //url: 'http://127.0.0.1:8080/api/login/',
-          url: 'http://10.198.94.76:8080/api/login/',
+          url: 'http://10.198.67.131:8088/api/login/',  
+          //url: 'http://127.0.0.1:8088/api/login/',
           method: 'POST',  
           data: {
           type:"camera",    
-          base1:that.data.code
+          base1:that.data.code,
+          methodChoice:store.methodChoice
           },
           success:  (res)=> {  
             console.log("POST请求成功")     
@@ -47,20 +69,75 @@ Page({
             that.setData({
               'result_base64': res.data.result_base64
             })
+
+            if(res.data.state == 1){
+              that.setData({
+                'request_allow': 1
+              })
+            }
+            else{
+              that.setData({
+                'request_allow': 0
+              })
+            }
           },  
           fail:(res)=> {  
-            console.log("POST请求失败",res)     
+            console.log("POST请求失败",res)
+            if(res.data.state == 1){
+              that.setData({
+                'request_allow': 1
+              })
+            }
+            else{
+              that.setData({
+                'request_allow': 0
+              })
+            }     
           },    
           });
+
+          }
+
           var ti = this.data.times
           if(ti == 0){
             clearInterval(timer) //点击停止检测时清除计时器timer
           }
-      },1000)//计时器的间隔时间，单位为ms
+      },3000)//计时器的间隔时间，单位为ms
   },
   stopPhoto:function(){
     var flag = 0
     this.data.times = flag
   },
+    /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    if (store.methodChoice == 0){
+      this.setData({
+        methodName: " rcnn " 
+      })
+    }
+    else if(store.methodChoice == 1){
+      this.setData({
+        methodName: " detectors " 
+      })
+    }
+    else{
+      this.setData({
+        methodName: " yolo " 
+      })
+    }
+
+  },
+    /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    var flag = 0
+    this.data.times = flag
+  },
+
+
+
 })
 
--- miniprogram-first/store/store.js
+++ miniprogram-first/store/store.js
@@ -5,14 +5,18 @@ import { observable, action } from 'mobx-miniprogram'
 export const store = observable({
   
   //数据字段
-  activeTabBarIndex:0,
+  activeTabBarIndex:0,//导航栏选中的标签索引
   ifLogin:0,//默认0未登录
+  methodChoice:0,//选中的检测方法 0：faster_rcnn   1：detectors_rcnn   2：yolo
 
   //计算属性
-
   //actions函数，专门来修改store中数据的值
   updateActiveTabBarIndex: action(function(index){
     this.activeTabBarIndex = index
+  }),
+  //更新store中的methodChoice变量
+  updateChoice:action(function(index){
+    this.methodChoice = index
   })
 
 })
